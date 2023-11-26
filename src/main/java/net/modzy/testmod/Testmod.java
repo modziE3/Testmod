28133c2eb1eae8ae0b9c5f1ae603a2fd05fc828d
Modzyyy <bluke136@gmail.com>
2023-11-26 18:08:12 +1300

    Initial commit

---
+++ src/main/java/net/modzy/testmod/Testmod.java
@@ -0,0 +1,16 @@
+package net.modzy.testmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Testmod implements ModInitializer {
+	public static final String MOD_ID = "testmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/modzy/testmod/TestmodClient.java
@@ -0,0 +1,10 @@
+package net.modzy.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
